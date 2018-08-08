3edbc099c2fc1f7f964a3a21aa94f12bc641b580
jipodine <jipodine@users.noreply.github.com>
2018-08-08 16:13:17 +0200

    Minor change for new API of streaming service

--- src/server/PlayerExperience.js
+++ src/server/PlayerExperience.js
@@ -8,9 +8,9 @@ export default class PlayerExperience extends Experience {
     this.sharedParams = this.require('shared-params');
 
     this.audioStreamManager = this.require('audio-stream-manager', {
-      audioFiles: [
-        'sounds/streams/stream-0.wav',
-      ],
+      audioFiles: {
+        'stream-0': 'sounds/streams/stream-0.wav',
+      },
     });
     // this.audioScheduler = this.require('audio-scheduler');
     this.syncScheduler = this.require('sync-scheduler');
